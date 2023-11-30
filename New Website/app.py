271ca729d7c3741d10ce43c5e1e48cb393f63b64
Nibir Das <das579nibir@gmail.com>
2023-11-30 21:22:19 +0530

    Custom

--- New Website/app.py
+++ New Website/app.py
@@ -1,26 +1,16 @@
-from flask import Flask, request, jsonify, render_template
-import csv
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
+def main_page():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    file.save('codon.csv')
-
-    # Read and print the contents of the CSV file
-    with open('codon.csv', 'r') as file:
-        csv_reader = csv.DictReader(file)
-        codon_usage = [row for row in csv_reader]
-        for row in codon_usage:
-            print(row)
-
-    return jsonify({'status': 'success', 'data': codon_usage})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5012)
+
+
 
